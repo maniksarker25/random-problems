210c1f9540d49e8b25dba6f21bea977482737492
manik <maniksarker652@gmail.com>
2025-12-03 23:17:21 +0600

    try to solve another problem

--- Differing_Values.cpp
+++ Differing_Values.cpp
@@ -6,7 +6,30 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
  
-    
-    
+    int t;
+    cin >>t;
+    while(t--){
+        int n,k;
+        cin >> n>>k;
+        string s;
+        cin >>s;
+        int zeroCount = 0,oneCount = 0;
+        for(int i = 0;i<n;i++){
+            if(s[i] == '0'){
+                zeroCount++;
+            }
+            else{
+                oneCount++;
+            }
+        }
+
+        if(max(oneCount,zeroCount) >  (n + k - 1) / k ){
+            cout << "NO"<<endl;
+        }
+        else{
+            cout <<"YES"<<endl;
+        }
+    }
+
     return 0;
 }
