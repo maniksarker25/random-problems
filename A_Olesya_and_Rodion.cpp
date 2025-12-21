df4a8102b3dd8fbfb45086784081b5895f38d4ee
manik <maniksarker652@gmail.com>
2025-12-21 22:10:06 +0600

    solve another problem

--- A_Olesya_and_Rodion.cpp
+++ A_Olesya_and_Rodion.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int n, t;
     cin >> n >> t;
 
-    if (n == 1 && t == 10) {
-        cout << -1; 
+    if (t == 10) {
+        if (n == 1) cout << -1;
+        else {
+            cout << 1;
+            for (int i = 1; i < n; i++) cout << 0;
+        }
     } else {
-        cout << t; 
-        for (int i = 1; i < n; i++) cout << 0; 
+        cout << t;
+        for (int i = 1; i < n; i++) cout << 0;
     }
 
+    cout << endl;
     return 0;
 }
