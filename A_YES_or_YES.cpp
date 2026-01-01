1ad1dd7c6bbea54bd8f65bc67250e83e43272666
manik <maniksarker652@gmail.com>
2026-01-01 21:27:01 +0600

    solve another problem

---
+++ A_YES_or_YES.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        for (char &c : s) {
+            c = tolower(c);
+        }
+
+        if (s == "yes")
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
